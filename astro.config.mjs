b3fbd9f16cb27345743d798dff1717d97b6cf9f3
Marcos Loureiro <mloureiro.dev@gmail.com>
2021-11-07 13:42:21 +0100

    Create script to make Github pages to work

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Enable the Vue renderer to support Vue components.
   renderers: ['@astrojs/renderer-vue'],
+  buildOptions: {
+    site: 'https://mloureiro.github.io',
+  },
 });
---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# ensure all code is committed to avoid unexpected live code
+if [ -n "$(git status -s)" ]; then
+  echo 'ERROR: there must not be any untracked or changed file'
+  echo 'Commit all your changes and try again'
+  exit 1
+fi
+
+# build
+npm run build
+
+# git to target branch
+git checkout gh-pages
+
+# replace the content with the dist/
+rm -rf $(git ls-tree --full-tree -r --name-only HEAD)
+git checkout -- .gitignore
+cp -Ra dist/* .
+
+# add .nojekyll to bypass GitHub Page's default behavior
+touch .nojekyll
+
+# commit and push changes
+git add .
+git commit -am 'Deploy'
+git push origin gh-pages
+
+# go bach to the original branch
+git checkout -
