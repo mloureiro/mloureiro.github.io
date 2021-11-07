c7d4d546c210da16d47bfef3f7cb320b4bca3113
Marcos Loureiro <mloureiro.dev@gmail.com>
2021-11-07 23:13:30 +0100

    Add profile-picture component for landing page

---
+++ src/components/profile-picture.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="profile-picture">
+		<img
+			src="/assets/profile-picture.png"
+			class="profile-picture __img"
+		/>
+	</div>
+</template>
+
+<style scoped>
+.profile-picture {
+	display: inline-flex;
+	overflow: hidden;
+	padding: var(--sp-xs);
+	border: var(--b-width-medium) solid var(--c-primary-bg);
+	border-radius: 100%;
+}
+.profile-picture.__img {
+	max-width: 100px;
+	max-height: 100px;
+	border-radius: 100%;
+	background-color: var(--c-primary-bg);
+}
+</style>
