e7794470076cad0abe02cf7a4e918c7fbadccdf5
Marcos Loureiro <mloureiro.dev@gmail.com>
2021-11-08 18:54:53 +0100

    Pass offset directly through style
    It isn't a great way to do it, but the SSR is causing issues with computed properties. I still have to figure it out why is this.

--- src/components/hexagon/hexagon-row.vue
+++ src/components/hexagon/hexagon-row.vue
@@ -1,32 +1,17 @@
 <template>
-	<div :style="style" class="hexagon-row">
+	<div class="hexagon-row">
 		<slot />
 	</div>
 </template>
 
-<script>
-export default {
-	props: {
-		offset: { type: Number, default: 0 },
-	},
-	computed: {
-		style() {
-			return this.offset
-				? `--hexagon-offset: ${this.offset}rem`
-				: null
-		},
-	},
-};
-</script>
-
 <style scoped>
 .hexagon-wrapper > .hexagon-row {
-	--hexagon-offset: 0rem;
+	--offset: 0rem;
 	display: flex;
-	margin-left: calc(4 * var(--hexagon-offset));
+	margin-left: calc(4 * var(--offset));
 }
 .hexagon-wrapper > .hexagon-row:nth-of-type(even) {
-	margin-left: calc(2rem + (4 * var(--hexagon-offset)));
+	margin-left: calc(2rem + (4 * var(--offset)));
 	margin-top: -1rem;
 }
 </style>
